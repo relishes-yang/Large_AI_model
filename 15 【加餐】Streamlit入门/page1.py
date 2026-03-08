8ecb88a983396b2385e7a47766f1896a85afcf96
relishes-yang <2859312928@qq.com>
2026-03-08 16:27:25 +0800

    Streamlit入门

---
+++ 15 【加餐】Streamlit入门/page1.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+st.title("我的个人网站 💡")
+"### 早上好！"
+a = 329 * 3
+a
+[11, 22, 33]
+{"a": "1", "b": "2", "c": 3}
+
+st.image("15 【加餐】Streamlit入门/头像.png", width=200)
+
+df = pd.DataFrame({"学号": ["01", "02", "03", "04", "05"],
+              "班级": ["二班", "一班", "二班", "三班", "一班"],
+              "成绩": [92, 67, 70, 88, 76]})
+st.dataframe(df)    # 表格选项
+st.divider()    # 分割线
+st.table(df)
